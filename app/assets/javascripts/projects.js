2925e5ce561894a385a035f3e33cc09a6fdf6dad
Mark <mystycs@gmail.com>
2016-10-24 22:21:31 -0400

    add delete project function

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,13 +1,32 @@
+// $(function() {
+//   $.getJSON( window.location.href + ".json",
+//     function(data) {
+//         title = '<h2>Project: ' + data.project.title + '</h2>';
+//         $(title).appendTo("#title");
+//
+//         description = '<h3>Description: ' + data.project.description + '</h3>';
+//         $(description).appendTo("#description");
+//
+//         postedBy = '<h5>Project By: ' + data.user + '</h5>' ;
+//         $(postedBy).appendTo("#postedBy");
+//     });
+// });
+
 $(function() {
   $.getJSON( window.location.href + ".json",
     function(data) {
-        title = '<h2>Project: ' + data.project.title + '</h2>';
-        $(title).appendTo("#title");
+        content = '<h2>Project: ' + data.project.title + '</h2>';
 
-        description = '<h3>Description: ' + data.project.description + '</h3>';
-        $(description).appendTo("#description");
+        content += '<h3>Description: ' + data.project.description + '</h3>';
 
-        postedBy = '<h5>Project By: ' + data.user + '</h5>' ;
-        $(postedBy).appendTo("#postedBy");
+        content += '<h5>Project By: ' + data.user + '</h5>' ;
+        $(content).appendTo("#project-content");
     });
 });
+
+function deleteProject() {
+  $.ajax({
+    url: window.location.href,
+    type: 'DELETE'
+  });
+};
